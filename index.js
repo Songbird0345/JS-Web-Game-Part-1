7d543d1e4a68940243a5708f995e227b12fec07c
Amanda Strong <amanda@theheebners.com>
2022-01-18 07:46:21 -0800

    Tree and Pillar Added

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-// function.newImage() {
-//     // let greenCharacter = document.createElement('img')
-//     // greenCharacter.src = 'assets/green-character.gif'
-//     // greenCharacter.style.position = 'fixed'
-//     // greenCharacter.style.left = '100px'
-//     // greenCharacter.style.bottom = '100px'
-//     // // document.body.append(greenCharacter)
+function newImage() {
+    let newImage = document.createElement('img')
+    position = 'fixed'
 
-//     // let pineTree = document.createElement('img')
-//     // pineTree.src = 'assets/pine-tree.png'
-//     // pineTree.style.position = 'fixed'
-//     // pineTree.style.left = '450px'
-//     // pineTree.style.bottom = '200px'
-//     // // document.body.append(pineTree)
+    greenCharacter {
+        greenCharacter.src = 'assets/green-character.gif'
+        style.left = '100px'
+        style.bottom = '100px'
+    }
+
+    pineTree {
+        pineTree.src = 'assets/pine-tree.png'
+        style.left = '450px'
+        style.bottom = '200px'
+    }
+
+    tree { 
+        tree.src = 'assets/tree.png'
+        style.left = '200px'
+        style.bottom = '300px'
+    }
+
+    pillar {
+        pillar.src = 'assets/pillar.png'
+        style.left ='350px'
+        style.bottom = '100px'
+    }
 
 
-// }
-// document.body.append(newImage) 
+    document.body.append(newImage)
+}
+
+
+
 
 // let greenCharacter = document.createElement('img')
 // greenCharacter.src = 'assets/green-character.gif'
@@ -30,3 +46,17 @@
 // pineTree.style.left = '450px'
 // pineTree.style.bottom = '200px'
 // document.body.append(pineTree)
+
+// let tree = document.createElement('img')
+// tree.src = 'assets/tree.png'
+// tree.style.position = 'fixed'
+// tree.style.left = '200px'
+// tree.style.bottom = '300px'
+// document.body.append(tree)
+
+// let pillar = document.createElement('img')
+// pillar.src = 'assets/pillar.png'
+// pillar.style.position = 'fixed'
+// pillar.style.left = '350px'
+// pillar.style.bottom = ' 100px'
+// document.body.append(pillar)
