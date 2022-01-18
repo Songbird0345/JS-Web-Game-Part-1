1b300cfb1052a3905ca0d0d615bb1226a4f97f10
Amanda Strong <amanda@theheebners.com>
2022-01-18 09:44:18 -0800

    Added Well

--- index.js
+++ index.js
@@ -30,50 +30,5 @@ newItem('assets/staff.png', '600px', '100px')
 newImage('assets/green-character.gif', '100px', '100px')
 newImage('assets/pillar.png', '350px', '100px')
 newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
 
-// let crate = document.createElement('img')
-// crate.src = 'assets/crate.png'
-// crate.style.position = 'fixed'
-// crate.style.left = '150px'
-// crate.style.bottom = '200px'
-// document.body.append(crate)
-
-// let well = document.createElement('img')
-// well.src = 'assets/well.png'
-// well.style.position = 'fixed'
-// well.style.left = '500px'
-// well.style.bottom = '425px'
-// document.body.append(well)
-
-// let sword = document.createElement('img')
-// sword.src = 'assets/sword.png'
-// sword.style.position = 'fixed'
-// sword.style.left = '500px'
-// sword.style.bottom = '405px'
-// document.body.append(sword)
-
-// sword.addEventListener('dblclick', function(){
-//     sword.remove()
-// })
-
-// let shield = document.createElement('img')
-// shield.src = 'assets/shield.png'
-// sword.style.position = 'fixed'
-// sword.style.left = '165px'
-// sword.style.bottom = '185px'
-// document.body.append(shield)
-
-// shield.addEventListener('dblclick', function(){
-//     shield.remove()
-// })
-
-// let staff = document.createElement('img')
-// staff.src = 'assets/staff.png'
-// staff.style.position = 'fixed'
-// staff.style.left = '600px'
-// staff.style.bottom = '100px'
-// document.body.append(staff)
-
-// staff.addEventListener('dblclick', function(){
-//     staff.remove()
-// })
